c25e3fb753580f8a5efc99f057dbf6a4fcb5723c
Laroshan Surendran <laroshanlaro@gmail.com>
2020-08-13 23:00:42 +0530

    completed

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 
 export default class TodoItem extends Component {
     render() {
+        const {title,handleDelete, handleEdit}=this.props;
+
         return (
-            <div>
-                <h3>TodoItem</h3>
-                <h7>git check</h7>
-            </div>
-        )
+             <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                 <h6>{title}</h6>
+             <div className="todo-icon">
+                 <span className="mx-2 text-success " onClick={handleEdit} >
+                     <i class="fas fa-pen    "></i>
+                 </span>
+                 <span className="mx-2 text-danger " onClick={handleDelete} >
+                     <i class="fas fa-trash    "></i>
+                 </span>
+             </div>
+             </li>
+        );
     }
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react'
 import TodoItem from './TodoItem';
 
 export default class TodoList extends Component {
+   
     render() {
+        const {items,clearlist,handleDelete,handleEdit}=this.props;
         return (
-            <div>
-<h4>TodoList</h4>
-<TodoItem/>
-            </div>
+                <ul class="list-group my-5">
+                    <h3 className="text-capitalize text-center">
+                        Todo List
+                    </h3>
+                        {
+                            items.map(item=>{
+                              return(
+                                  <TodoItem
+                                  key={item.id}
+                                  title={item.title}
+                                  handleDelete={()=>handleDelete(item.id)}
+                                  handleEdit={()=>handleEdit(item.id)}
+                                  />
+
+                                  
+                              );  
+                            })
+                        }
+                        <button type="button" className="btn btn-danger btn-block
+                        text-uppercase mt-5" onClick={clearlist}>
+                            clear list
+                        </button>
+
+                </ul>
         )
     }
 }
